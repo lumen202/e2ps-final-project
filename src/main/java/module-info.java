a0c6b5d5a38d579486ec13d3717d6e12a8dfe127
lumen202 <jdiniega202@gmail.com>
2025-02-05 14:26:43 +0800

    base state

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,21 @@
 module dev.finalproject {
-    requires javafx.controls;
+
+    requires transitive javafx.controls;
+    requires transitive core.fx;
     requires javafx.fxml;
+    requires atlantafx.base;
+    requires javafx.graphics;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign2;
+    requires org.kordamp.ikonli.materialdesign;
+
+    requires transitive core.db;
+    requires core.util;
+    requires java.sql.rowset;
+    requires javafx.base;
 
     opens dev.finalproject to javafx.fxml;
+
     exports dev.finalproject;
 }
