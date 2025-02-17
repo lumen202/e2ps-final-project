a37645b84231d3af410afa6180bbde6bc26b8629
lumen202 <jdiniega202@gmail.com>
2025-02-17 09:46:53 +0800

    database link fix

--- src/main/java/dev/finalproject/App.java
+++ src/main/java/dev/finalproject/App.java
@@ -23,7 +23,7 @@ public class App extends FXApplication {
         public static final FXNodeRegister NODE_REGISTER = FXNodeRegister.INSTANCE;
 
         public static final DBService DB_SMS = DBService.INSTANCE
-                        .initialize("jdbc:mysql://localhost/student_management_system_db?user=root&password=");
+                        .initialize("jdbc:mysql://192.168.254.108:3306/student_management_system_db?user=root&password=admin&allowPublicKeyRetrieval=true&useSSL=false");
 
         @Override
         public void initialize() throws Exception {
